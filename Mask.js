c6029e5e7ae6c0ebd72560331fa19e58ad99cfa6
MASK SER <94510246+mask-sir@users.noreply.github.com>
2022-08-10 08:42:59 +0530

    Fix

---
+++ Mask.js
@@ -0,0 +1,11 @@
+//_______________________ ┏  Info  ┓ _______________________\\
+//
+//   Credit : AlipBot
+//   
+//   Note 
+//   Do not sell this SC,
+// Do not remove this text,
+// Who Wants To Upload Don't Forget Credit :),
+// Those Who Don't Put Credit Will Take Action
+//   
+//_______________________ ┏ MADE BY MASK SER ┓ _______________________\\
