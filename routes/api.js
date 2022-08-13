11cc86aa1fb3c879186cd84be925a172e5a86005
MASK SER <94510246+mask-sir@users.noreply.github.com>
2022-08-13 21:43:20 +0530

    Fix obf

--- routes/api.js
+++ routes/api.js
@@ -935,13 +935,12 @@ console.log('receiving data ...');
 if (!code) return res.json({status: false, creator: `${creator}`, message: "No Code Given"})
 var JavaScriptObfuscator = require('javascript-obfuscator');
 var obfuscationResult = JavaScriptObfuscator.obfuscate(code,value);
-res.json({status: 200, creator: `${creator}`, code: `${JSON.parse(obfuscationResult.getObfuscatedCode())}`})
+res.json({status: 200, creator: `${creator}`, code: `${obfuscationResult.getObfuscatedCode()}`})
 } catch (err) {
 console.log(err)
 res.json({status: 404, creator: `${creator}`, code: 'error in code'})
 }
 })
-
 //―――――――――――――――――――――――――――――――――――――――――― ┏  Random Gambar ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
 
 
